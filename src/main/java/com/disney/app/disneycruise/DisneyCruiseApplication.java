851e9a311b9f7e24396b10b72bab1dfe3d35a91c
Ricardo Lima <ricardoltlima@gmail.com>
2026-06-16 12:40:06 -0400

    First commit

---
+++ src/main/java/com/disney/app/disneycruise/DisneyCruiseApplication.java
@@ -0,0 +1,13 @@
+package com.disney.app.disneycruise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisneyCruiseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DisneyCruiseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/disney/app/disneycruise/DisneyCruiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.disney.app.disneycruise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisneyCruiseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
